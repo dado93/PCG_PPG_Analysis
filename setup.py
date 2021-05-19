dfbf25fbe3ad20c7905b1c61269c9c94c3b58d33
dado93 <davide.marzorati.93@gmail.com>
2021-05-19 10:57:47 +0200

    Added requirements for installation

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import setuptools
 
+requirements = ['pandas>1.1']
+
 config = {
-        'name': "pcg_ppg_analysis",
-        'version': "0.1",
-        'author': "Davide Marzorati",
-        'author_email': "davide.marzorati@polimi.it",
-        'packages': setuptools.find_packages(),
+    'name': "pcg_ppg_analysis",
+    'version': "0.1",
+    'author': "Davide Marzorati",
+    'author_email': "davide.marzorati@polimi.it",
+    'packages': setuptools.find_packages(),
 }
 
 setuptools.setup(**config)
